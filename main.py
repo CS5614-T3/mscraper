8a9a7d15c6e7f8afdebefb4fc11ef0c12e7e225d
Scott Ruona <sfr@vt.edu>
2026-04-09 21:38:32 -0400

    Get response from Instances API and load into Supabase

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import os
 from dotenv import dotenv_values
 from supabase import create_client, Client
 
@@ -6,16 +7,31 @@ from src.instances import InstancesAPI
 
 
 def main():
-    get_instances()
+    config = dotenv_values(".env")
+    url: str | None = config["SUPABASE_URL"]
+    key: str | None = config["SUPABASE_KEY"]
+
+    if url is None or key is None:
+        raise ValueError("No Supabase url or key provided")
+
+    supabase: Client = create_client(url, key)
+
+    instances = get_instances()["instances"]
+    # Unpack the info object into the main object
+    [instance.update(instance.pop("info", None)) for instance in instances]
+
+
+    try:
+        response = supabase.table("instances_raw").upsert(instances).execute()
+    except Exception as exception:
+        print(exception)
 
 
 def get_instances():
     config = dotenv_values(".env")
     instances_api = InstancesAPI(config["INSTANCES_SECRET"])
-    instances = instances_api.list_instances(1, "false", "false", 100, "", "").json()
-    file = open("instances.json", mode="w")
-    json.dump(instances, file, indent=2)
-
+    instances = instances_api.list_instances(100, "false", "false", 100, "", "").json()
+    return instances
 
 if __name__ == "__main__":
     main()
